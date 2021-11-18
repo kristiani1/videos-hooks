32aab30f205729a2a8114a54deb8b96be2c3e049
kristiani1 <64782305+kristiani1@users.noreply.github.com>
2021-11-18 19:17:58 +0200

    add

---
+++ src/hooks/useVideos.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect} from 'react'
+import Youtube from '../apis/Youtube'
+
+const useVideos = (defaultSearchTerm) => {
+    const [videos, setVideos] = useState([])
+
+    useEffect(() => {
+        search(defaultSearchTerm)
+    }, [defaultSearchTerm])
+
+    const search = async term => {
+        const response = await Youtube.get('/search', {
+            params: {
+                q: term
+            }
+        })
+        setVideos(response.data.items)
+    }
+    return [videos, search]
+}
+
+export default useVideos
